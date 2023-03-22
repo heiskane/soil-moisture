3df5fe0f1630476341fabb59251d3df861452505
heiskane <henipe@gmail.com>
2023-03-22 18:43:51 +0200

    implement reading demultiplexer

--- esp8266-controller/esp8266-controller.ino
+++ esp8266-controller/esp8266-controller.ino
@@ -10,9 +10,16 @@ const char* password = "SECRET_HERE";
 const char* mqtt_server = "192.168.1.94";
 
 const int analogInPin = A0;  // ESP8266 Analog Pin ADC0 = A0
+const int S0 = 16;           // D0
+const int S1 = 5;            // D1
+const int S2 = 4;            // D2
+const int mux_enable = 0;    // D4
+
 int sensor_value = 0;
+int mux_channels = 4;
 
 DynamicJsonDocument msg(512);
+
 void reconnect() {
   // Loop until we're reconnected
   while (!mqtt_client.connected()) {
@@ -48,25 +55,51 @@ void setup() {
   Serial.print("IP Address is: ");
   Serial.println(WiFi.localIP());
 
-  mqtt_client.setServer(mqtt_server, 1883);
-  reconnect();
+  //mqtt_client.setServer(mqtt_server, 1883);
+  //reconnect();
+
+  pinMode(mux_enable, OUTPUT);
+  pinMode(S0, OUTPUT);
+  pinMode(S1, OUTPUT);
+  pinMode(S2, OUTPUT);
+  pinMode(analogInPin, INPUT);
+
+  digitalWrite(mux_enable, LOW);
+  digitalWrite(S0, LOW);
+  digitalWrite(S1, LOW);
+  digitalWrite(S2, LOW);
 }
 
 void loop() {
-  sensor_value = analogRead(analogInPin);
-  Serial.print("sensor_value: ");
-  Serial.println(sensor_value);
 
+  Serial.println();
+  
+  for (int i = 0; i < mux_channels; i++) {    
+    digitalWrite(S0, bitRead(i, 0));
+    digitalWrite(S1, bitRead(i, 1));
+    digitalWrite(S2, bitRead(i, 2));
 
-  if (!mqtt_client.connected()) {
-    reconnect();
+    sensor_value = analogRead(analogInPin);
+    Serial.print("sensor");
+    Serial.print(i + 1);
+    Serial.print("_value: ");
+    Serial.println(sensor_value);
+
+    delay(300);
   }
 
-  char tempString[512];
-  msg["sensor"] = "test-sensor";
-  msg["value"] = sensor_value;
-  serializeJson(msg, tempString);
 
-  mqtt_client.publish("soil-moisture", tempString);
+
+
+  //if (!mqtt_client.connected()) {
+  //  reconnect();
+  //}
+
+  //char tempString[512];
+  //msg["sensor"] = "test-sensor";
+  //msg["value"] = sensor_value;
+  //serializeJson(msg, tempString);
+
+  //mqtt_client.publish("soil-moisture", tempString);
   delay(1000);
 }
